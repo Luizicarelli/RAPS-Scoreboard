487926f2d27753eeef584e52539717dfee6340c1
teacherIan <90660856+teacherIan@users.noreply.github.com>
2022-06-20 11:14:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -45,15 +45,15 @@ fetch('https://house-points-db.herokuapp.com/get')
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    houses[1].totalPoints = +data[0].points;
-    houses[0].totalPoints = +data[1].points;
-    houses[2].totalPoints = +data[2].points;
-    houses[3].totalPoints = +data[3].points;
+    houses[1].totalPoints = +data[1].points;
+    houses[0].totalPoints = +data[0].points;
+    houses[2].totalPoints = +data[3].points;
+    houses[3].totalPoints = +data[2].points;
 
-    houses[1].winningMessage = data[0].message;
-    houses[0].winningMessage = data[1].message;
-    houses[2].winningMessage = data[2].message;
-    houses[3].winningMessage = data[3].message;
+    houses[1].winningMessage = data[1].message;
+    houses[0].winningMessage = data[0].message;
+    houses[2].winningMessage = data[3].message;
+    houses[3].winningMessage = data[2].message;
 
     rubyPoints.innerHTML = houses[0].totalPoints;
     sapphirePoints.innerHTML = houses[1].totalPoints;
